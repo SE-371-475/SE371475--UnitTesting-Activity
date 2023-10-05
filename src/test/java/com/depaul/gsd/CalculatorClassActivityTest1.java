91a81809978d973282932154d68c9bc72659d82d
AbeBV <aberkley@depaul.edu>
2023-10-05 16:03:27 -0500

    Added test cases

---
+++ src/test/java/com/depaul/gsd/CalculatorClassActivityTest1.java
@@ -0,0 +1,21 @@
+package com.depaul.gsd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorClassActivityTest1 {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+}
