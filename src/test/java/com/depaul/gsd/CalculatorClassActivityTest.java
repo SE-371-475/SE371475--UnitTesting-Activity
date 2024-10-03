1206a241ebbdc4a9f389726105f9a018bd4474ea
Ahmed Huzair Razi <ahmed.huzair70@gmail.com>
2024-10-02 20:09:45 -0500

    Wrote test cases

--- src/test/java/com/depaul/gsd/CalculatorClassActivityTest.java
+++ src/test/java/com/depaul/gsd/CalculatorClassActivityTest.java
@@ -1,4 +1,38 @@
+package com.depaul.gsd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorClassActivityTest {
-  
+
+    private CalculatorClassActivity calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new CalculatorClassActivity();
+    }
+
+    @Test
+    @DisplayName("Adding 7 to the initial value")
+    void add() {
+        calculator.add(7);
+        assertEquals(7, calculator.getValue(), "Adding 7 should return 7.");
+    }
+
+    @Test
+    @DisplayName("Subtracting 10 from the initial value")
+    void subtract() {
+        calculator.subtract(10);
+        assertEquals(-10, calculator.getValue(), "Subtracting 10 should return -10.");
+    }
+
+    @Test
+    @DisplayName("Confirm initial value is 0")
+    void getValue() {
+        assertEquals(0, calculator.getValue(), "Initial value should be 0.");
+
+    }
 }
