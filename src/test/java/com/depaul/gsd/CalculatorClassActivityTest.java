72f46c65bc5a94df519f5fa5c0200a0f758bf5fc
Vahid Alizadeh <vahid.alizadeh@gmail.com>
2024-10-02 20:15:30 -0500

    my test cases

---
+++ src/test/java/com/depaul/gsd/CalculatorClassActivityTest.java
@@ -0,0 +1,36 @@
+package com.depaul.gsd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorClassActivityTest {
+
+    CalculatorClassActivity calculator;
+    @BeforeEach
+    void setUp() {
+        calculator = new CalculatorClassActivity();
+    }
+
+    @Test
+    @DisplayName("Confirm adding 7 to initial value")
+    void add() {
+        calculator.add(7);
+        assertEquals(calculator.getValue(), 7);
+    }
+
+    @Test
+    @DisplayName("Confirm subtracting 10 from the initial value")
+    void subtract() {
+        calculator.subtract(10);
+        assertEquals(calculator.getValue(), -10);
+    }
+
+    @Test
+    @DisplayName("Confirm the initial value is 0")
+    void getValue() {
+        assertEquals(calculator.getValue(), 0);
+    }
+}
