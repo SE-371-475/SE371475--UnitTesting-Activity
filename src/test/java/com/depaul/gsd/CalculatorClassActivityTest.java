a15f3af1a9484d848ef251458f25b42194459942
Aura <aluna36@depaul.edu>
2024-10-02 19:56:56 -0500

    Created CalculatorClassActivityTest Class

---
+++ src/test/java/com/depaul/gsd/CalculatorClassActivityTest.java
@@ -0,0 +1,41 @@
+package com.depaul.gsd;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Aura: method with CCA")
+class CalculatorClassActivityTest {
+    CalculatorClassActivity calculatorClassActivity;
+
+    @BeforeEach
+    void setUp() { calculatorClassActivity = new CalculatorClassActivity(); }
+
+    @Test
+    @Order(1)
+    @DisplayName("Ensures that inital value is 0")
+    void initiateValue() {
+        assertEquals(0, calculatorClassActivity.getValue());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Tests addition method")
+    void adding() {
+        calculatorClassActivity.add(7);
+        int expected = 7;
+        int actual = calculatorClassActivity.getValue();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test subtraction method")
+    void subtracting() {
+        calculatorClassActivity.subtract(10);
+        int expected = -10;
+        int actual = calculatorClassActivity.getValue();
+
+        assertEquals(expected, actual);
+    }
+
+}
