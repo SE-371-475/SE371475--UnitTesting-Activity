2b6d1188b1c277114dff947a23b9d546c6bc08a6
lburdic2 <lburdic2@depaul.edu>
2024-10-06 11:05:48 -0500

    Added tests for initial value, addition, and subtraction

--- src/test/java/com/depaul/gsd/CalculatorClassActivityTest.java
+++ src/test/java/com/depaul/gsd/CalculatorClassActivityTest.java
@@ -1,48 +1,42 @@
 package com.depaul.gsd;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-@DisplayName("Lamees: CalculatorClassActivityTest")
+
+@DisplayName("Burdick: Checking initial value, add, subtract, and getValue")
 class CalculatorClassActivityTest {
     CalculatorClassActivity calculator;
-
     @BeforeEach
     void setUp() {
-      calculator = new CalculatorClassActivity();
-    }
-    @Test
-    @DisplayName("Initial value should be 0")
-    void isInitialValueZero() {
-        int expected = 0;
-        int actual = calculator.getValue();
-        assertEquals(expected,actual,"initial value should be 0");
-
-
+        calculator=new CalculatorClassActivity();
     }
 
     @Test
+    @DisplayName("Ensure initial value of calculator should be zero")
+    void initialValue() {
+        int expected=0;
+        int actual=calculator.getValue();
+        assertEquals(expected, actual, "Initial value for calculator should be zero");
+    }
+
+    @Test
+    @DisplayName("Ensure addition works")
     void add() {
-        int expected = 7;
+        int expected=7;
         calculator.add(7);
-        int actual = calculator.getValue();
-        assertEquals(expected,actual, "result should be 7");
+        int actual=calculator.getValue();
+        assertEquals(expected, actual, "Calculator should have value of 7 after adding 7");
     }
 
     @Test
+    @DisplayName("Ensure subtraction works")
     void subtract() {
-        int expected = -10;
+        int expected=-10;
         calculator.subtract(10);
-        int actual = calculator.getValue();
-        assertEquals(expected,actual,"should return -10");
+        int actual=calculator.getValue();
+        assertEquals(expected, actual, "Calculator should have value of 10 after subtracting 10");
     }
 
-    @Test
-    void getValue() {
-    }
 }
-
-
-
---
+++ src/test/java/com/depaul/gsd/CalculatorTestAUTO.java
@@ -0,0 +1,12 @@
+package com.depaul.gsd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTestAUTO {
+
+    @Test
+    void multiply() {
+    }
+}
